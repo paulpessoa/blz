5cc930fb8aac5e916b627ef0582c7aa2232c5cd1
Paul Pessoa <paulmspessoa@gmail.com>
2024-10-12 12:29:52 -0300

    a

--- main.js
+++ main.js
@@ -4,6 +4,24 @@ document.addEventListener("DOMContentLoaded", () => {
   const directionButton = document.getElementById("direction-button");
   const direction = document.getElementById("direction");
 
+  const themeToggle = document.getElementById("theme-toggle");
+  const body = document.body;
+
+  // Função para trocar de tema
+  function toggleTheme() {
+    const currentTheme = body.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    body.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+  }
+
+  // Carregar tema salvo no localStorage
+  const savedTheme = localStorage.getItem("theme") || "light"; // padrão para 'light'
+  body.setAttribute("data-theme", savedTheme);
+
+  // Lidar com o clique no botão de toggle de temas
+  themeToggle.addEventListener("click", toggleTheme);
+
   geoButton.addEventListener("click", () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
